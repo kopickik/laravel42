dce89ae06f2eb583d4bccbe204bc8f3027ada136
Kyle Kopicki <kkopicki@Kyles-MacBook-Pro.local>
2015-03-09 02:33:45 -0400

    getting codecept test bck to green

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -6,4 +6,11 @@
 		<p class="lead">A chance to refine your ideas of what a developed application <em>should be</em>.</p>
 		{{ link_to_route('register_path', 'Sign Up', null, ['class' => 'btn btn-lg btn-primary']) }}
 	</div>
+	<div class="col-sm-6 animated idle">
+		<ul class="nav">
+			<li>
+				<a href="#">a hidden realm</a>
+			</li>
+		</ul>
+	</div>
 @stop
--- tests/functional/SignUpCept.php
+++ tests/functional/SignUpCept.php
@@ -8,8 +8,8 @@ $I->amOnPage('/');
 $I->click('Sign Up');
 $I->seeCurrentUrlEquals('/register');
 
-$I->fillField('Username:', 'JohnDoe2');
-$I->fillField('Email:', 'JohnDoe2@example.com');
+$I->fillField('Username:', 'JohnDoe3');
+$I->fillField('Email:', 'JohnDoe3@example.com');
 $I->fillField('Password:', 'secret2');
 $I->fillField('Password Confirmation:', 'secret2');
 $I->click('Register');
@@ -18,8 +18,8 @@ $I->seeCurrentUrlEquals('');
 $I->see('Welcome to Larabook.');
 
 $I->seeRecord('users', [
-	'username' => 'JohnDoe',
-	'email' => 'JohnDoe@example.com'
+	'username' => 'JohnDoe3',
+	'email' => 'JohnDoe3@example.com'
 	]);
 
 $I->assertTrue(Auth::check());
